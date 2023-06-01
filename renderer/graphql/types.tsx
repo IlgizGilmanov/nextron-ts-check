85b2c150aa2efa269c3a384fbc8e604b99de1b15
Ilgiz Gilmanov <ilgiz.gilmanov@flatstack.com>
2023-06-01 13:34:57 +0200

    Copied config and graphql folders from the ChatterAI repo

--- renderer/graphql/types.tsx
+++ renderer/graphql/types.tsx
@@ -13,50 +13,79 @@ export type Scalars = {
   ISO8601DateTime: string;
 };
 
-/** Data type for Activity */
-export type Activity = {
-  __typename?: 'Activity';
-  /** Body */
-  body: Scalars['String'];
-  /** Created datetime */
-  createdAt: Scalars['ISO8601DateTime'];
-  /** Event type from Enum */
-  event: ActivityEvent;
+/** Input object for chat archive mutation */
+export type ArchiveChatInput = {
   /** ID */
   id: Scalars['ID'];
-  /** Title */
-  title: Scalars['String'];
-  /** Activity initiator */
-  user: User;
 };
 
-/** The connection type for Activity. */
-export type ActivityConnection = {
-  __typename?: 'ActivityConnection';
+/** Payload object for archive chat mutation */
+export type ArchiveChatPayload = {
+  __typename?: 'ArchiveChatPayload';
+  /** Chat */
+  chat: Chat;
+};
+
+export type AuthorizationResult = {
+  __typename?: 'AuthorizationResult';
+  /** Human-readable error message */
+  message?: Maybe<Scalars['String']>;
+  /** Reasons of check failure */
+  reasons?: Maybe<FailureReasons>;
+  /** Result of applying a policy rule */
+  value: Scalars['Boolean'];
+};
+
+/** Data type for Chat */
+export type Chat = {
+  __typename?: 'Chat';
+  canArchive: AuthorizationResult;
+  canCreateMessage: AuthorizationResult;
+  canUpdate: AuthorizationResult;
+  /** Created datetime */
+  createdAt: Scalars['ISO8601DateTime'];
+  /** ID */
+  id: Scalars['ID'];
+  /** Messages */
+  messages: Array<Message>;
+  /** Name */
+  name: Scalars['String'];
+  /** Speech style */
+  speechStyle?: Maybe<SpeechStyle>;
+  /** Status */
+  status: ChatStatus;
+  /** Tone of voice */
+  toneOfVoice?: Maybe<ToneOfVoice>;
+  /** Created datetime */
+  updatedAt: Scalars['ISO8601DateTime'];
+};
+
+/** The connection type for Chat. */
+export type ChatConnection = {
+  __typename?: 'ChatConnection';
   /** A list of edges. */
-  edges?: Maybe<Array<Maybe<ActivityEdge>>>;
+  edges?: Maybe<Array<Maybe<ChatEdge>>>;
   /** A list of nodes. */
-  nodes?: Maybe<Array<Maybe<Activity>>>;
+  nodes?: Maybe<Array<Maybe<Chat>>>;
   /** Information to aid in pagination. */
   pageInfo: PageInfo;
 };
 
 /** An edge in a connection. */
-export type ActivityEdge = {
-  __typename?: 'ActivityEdge';
+export type ChatEdge = {
+  __typename?: 'ChatEdge';
   /** A cursor for use in pagination. */
   cursor: Scalars['String'];
   /** The item at the end of the edge. */
-  node?: Maybe<Activity>;
+  node?: Maybe<Chat>;
 };
 
-/** Data type for Activity Event */
-export enum ActivityEvent {
-  ResetPasswordRequested = 'RESET_PASSWORD_REQUESTED',
-  UserLoggedIn = 'USER_LOGGED_IN',
-  UserRegistered = 'USER_REGISTERED',
-  UserResetPassword = 'USER_RESET_PASSWORD',
-  UserUpdated = 'USER_UPDATED',
+/** Chat status */
+export enum ChatStatus {
+  /** Generating ai response message */
+  Generating = 'GENERATING',
+  /** Ai response message generated */
+  Ready = 'READY',
 }
 
 /** Input object for user confirmation flow */
@@ -72,13 +101,44 @@ export type ConfirmUserPayload = {
   me: CurrentUser;
 };
 
+/** Input object for chat creation mutation */
+export type CreateChatInput = {
+  /** Message to create with chat */
+  message?: InputMaybe<MessageInput>;
+  /** Chat name */
+  name?: InputMaybe<Scalars['String']>;
+};
+
+/** Payload object for create chat mutation */
+export type CreateChatPayload = {
+  __typename?: 'CreateChatPayload';
+  /** Chat */
+  chat: Chat;
+};
+
+/** Input object for message creation mutation */
+export type CreateMessageInput = {
+  /** Chat ID */
+  chatId: Scalars['ID'];
+  /** First message content */
+  content: Scalars['String'];
+};
+
+/** Payload object for create chat mutation */
+export type CreateMessagePayload = {
+  __typename?: 'CreateMessagePayload';
+  /** Message */
+  message: Message;
+};
+
 /** Data type for Current User */
 export type CurrentUser = {
   __typename?: 'CurrentUser';
-  /** Activities array */
-  activities?: Maybe<ActivityConnection>;
   /** URL for avatar image */
   avatarUrl?: Maybe<Scalars['String']>;
+  canCreateChat: AuthorizationResult;
+  /** Chats array */
+  chats?: Maybe<ChatConnection>;
   /** Confirmation datetime */
   confirmedAt?: Maybe<Scalars['ISO8601DateTime']>;
   /** Email */
@@ -92,12 +152,19 @@ export type CurrentUser = {
 };
 
 /** Data type for Current User */
-export type CurrentUserActivitiesArgs = {
+export type CurrentUserChatsArgs = {
   after?: InputMaybe<Scalars['String']>;
   before?: InputMaybe<Scalars['String']>;
-  events?: InputMaybe<Array<ActivityEvent>>;
   first?: InputMaybe<Scalars['Int']>;
   last?: InputMaybe<Scalars['Int']>;
+};
+
+export type FailureReasons = {
+  __typename?: 'FailureReasons';
+  /** JSON-encoded map of reasons */
+  details: Scalars['String'];
+  /** Human-readable errors */
+  fullMessages: Array<Scalars['String']>;
 };
 
 /** Data type for file direct upload */
@@ -120,25 +187,76 @@ export type ImageUploaderMetadata = {
   size: Scalars['Int'];
 };
 
+/** Data type for Message */
+export type Message = {
+  __typename?: 'Message';
+  canArchive: AuthorizationResult;
+  canUpdate: AuthorizationResult;
+  /** Chat */
+  chat: Chat;
+  /** Content */
+  content: Scalars['String'];
+  /** Created datetime */
+  createdAt: Scalars['ISO8601DateTime'];
+  /** ID */
+  id: Scalars['ID'];
+  /** Role */
+  role: MessageRole;
+  /** Created datetime */
+  updatedAt: Scalars['ISO8601DateTime'];
+};
+
+/** Input object for message creation mutation */
+export type MessageInput = {
+  /** First message content */
+  content: Scalars['String'];
+};
+
+/** Message role */
+export enum MessageRole {
+  /** Assistant message */
+  Assistant = 'ASSISTANT',
+  /** Error message */
+  Error = 'ERROR',
+  /** User message */
+  User = 'USER',
+}
+
 /** Mutations */
 export type Mutation = {
   __typename?: 'Mutation';
+  /** Archive chat */
+  archiveChat?: Maybe<ArchiveChatPayload>;
   /** Confirm user email */
   confirmUser?: Maybe<ConfirmUserPayload>;
+  /** Create chat */
+  createChat?: Maybe<CreateChatPayload>;
+  /** Create message */
+  createMessage?: Maybe<CreateMessagePayload>;
   /** Authenticate with OAuth */
   omniauthSignInOrSignUp?: Maybe<SignInPayload>;
   /** File presign data for upload */
   presignData?: Maybe<PresignDataPayload>;
-  /** Reset password request */
+  /** Request password recovery link in email */
   requestPasswordRecovery?: Maybe<RequestPasswordRecoveryPayload>;
+  /** Request password recovery short code in email */
+  requestPasswordRecoveryShortCode?: Maybe<RequestPasswordRecoveryShortCodePayload>;
+  /** Reset user's password with short code */
+  resetPasswordWithShortCode?: Maybe<ResetPasswordWithShortCodePayload>;
+  /** Reset user's password with reset token */
+  resetPasswordWithToken?: Maybe<ResetPasswordWithTokenPayload>;
+  /** Set chat speech style */
+  setChatSpeechStyle?: Maybe<SetChatSpeechStylePayload>;
+  /** Set chat tone of voice */
+  setChatToneOfVoice?: Maybe<SetChatToneOfVoicePayload>;
   /** Sign in */
   signIn?: Maybe<SignInPayload>;
   /** Sign out */
   signOut?: Maybe<SignOutPayload>;
   /** Sign up */
   signUp?: Maybe<SignUpPayload>;
-  /** Update user password */
-  updatePassword?: Maybe<UpdatePasswordPayload>;
+  /** Update chat */
+  updateChat?: Maybe<UpdateChatPayload>;
   /** Update short live access token */
   updateToken?: Maybe<UpdateTokenPayload>;
   /** Update user info */
@@ -146,8 +264,23 @@ export type Mutation = {
 };
 
 /** Mutations */
+export type MutationArchiveChatArgs = {
+  input: ArchiveChatInput;
+};
+
+/** Mutations */
 export type MutationConfirmUserArgs = {
   input: ConfirmUserInput;
+};
+
+/** Mutations */
+export type MutationCreateChatArgs = {
+  input: CreateChatInput;
+};
+
+/** Mutations */
+export type MutationCreateMessageArgs = {
+  input: CreateMessageInput;
 };
 
 /** Mutations */
@@ -166,6 +299,31 @@ export type MutationRequestPasswordRecoveryArgs = {
 };
 
 /** Mutations */
+export type MutationRequestPasswordRecoveryShortCodeArgs = {
+  input: RequestPasswordRecoveryShortCodeInput;
+};
+
+/** Mutations */
+export type MutationResetPasswordWithShortCodeArgs = {
+  input: ResetPasswordWithShortCodeInput;
+};
+
+/** Mutations */
+export type MutationResetPasswordWithTokenArgs = {
+  input: ResetPasswordWithTokenInput;
+};
+
+/** Mutations */
+export type MutationSetChatSpeechStyleArgs = {
+  input: SetChatSpeechStyleInput;
+};
+
+/** Mutations */
+export type MutationSetChatToneOfVoiceArgs = {
+  input: SetChatToneOfVoiceInput;
+};
+
+/** Mutations */
 export type MutationSignInArgs = {
   input: SignInInput;
 };
@@ -181,8 +339,8 @@ export type MutationSignUpArgs = {
 };
 
 /** Mutations */
-export type MutationUpdatePasswordArgs = {
-  input: UpdatePasswordInput;
+export type MutationUpdateChatArgs = {
+  input: UpdateChatInput;
 };
 
 /** Mutations */
@@ -242,52 +400,11 @@ export type PresignField = {
   value: Scalars['String'];
 };
 
-/** Data type for Public Activity */
-export type PublicActivity = {
-  __typename?: 'PublicActivity';
-  /** Body */
-  body: Scalars['String'];
-  /** ID */
-  id: Scalars['ID'];
-  /** Title */
-  title: Scalars['String'];
-};
-
-/** The connection type for PublicActivity. */
-export type PublicActivityConnection = {
-  __typename?: 'PublicActivityConnection';
-  /** A list of edges. */
-  edges?: Maybe<Array<Maybe<PublicActivityEdge>>>;
-  /** A list of nodes. */
-  nodes?: Maybe<Array<Maybe<PublicActivity>>>;
-  /** Information to aid in pagination. */
-  pageInfo: PageInfo;
-};
-
-/** An edge in a connection. */
-export type PublicActivityEdge = {
-  __typename?: 'PublicActivityEdge';
-  /** A cursor for use in pagination. */
-  cursor: Scalars['String'];
-  /** The item at the end of the edge. */
-  node?: Maybe<PublicActivity>;
-};
-
 /** Base query */
 export type Query = {
   __typename?: 'Query';
-  /** Activities */
-  activities?: Maybe<PublicActivityConnection>;
   /** Current User */
   me?: Maybe<CurrentUser>;
-};
-
-/** Base query */
-export type QueryActivitiesArgs = {
-  after?: InputMaybe<Scalars['String']>;
-  before?: InputMaybe<Scalars['String']>;
-  first?: InputMaybe<Scalars['Int']>;
-  last?: InputMaybe<Scalars['Int']>;
 };
 
 /** Input object for reset password flow */
@@ -303,6 +420,89 @@ export type RequestPasswordRecoveryPayload = {
   detail: Scalars['String'];
   /** Message */
   message: Scalars['String'];
+};
+
+/** Input object for reset password flow */
+export type RequestPasswordRecoveryShortCodeInput = {
+  /** Email */
+  email: Scalars['String'];
+};
+
+/** Payload object for request password reset mutation */
+export type RequestPasswordRecoveryShortCodePayload = {
+  __typename?: 'RequestPasswordRecoveryShortCodePayload';
+  /** Detail */
+  detail: Scalars['String'];
+  /** Message */
+  message: Scalars['String'];
+};
+
+/** Input object for update user password on reset password */
+export type ResetPasswordWithShortCodeInput = {
+  /** A new password */
+  password: Scalars['String'];
+  /** Password reset short code */
+  shortCode: Scalars['String'];
+};
+
+/** Payload object for update password mutation */
+export type ResetPasswordWithShortCodePayload = {
+  __typename?: 'ResetPasswordWithShortCodePayload';
+  /** Short live access token */
+  accessToken: Scalars['String'];
+  /** Current User */
+  me?: Maybe<CurrentUser>;
+  /** Long live refresh token */
+  refreshToken: Scalars['String'];
+};
+
+/** Input object for update user password on reset password */
+export type ResetPasswordWithTokenInput = {
+  /** A new password */
+  password: Scalars['String'];
+  /** Password reset token */
+  resetToken: Scalars['String'];
+};
+
+/** Payload object for update password mutation */
+export type ResetPasswordWithTokenPayload = {
+  __typename?: 'ResetPasswordWithTokenPayload';
+  /** Short live access token */
+  accessToken: Scalars['String'];
+  /** Current User */
+  me?: Maybe<CurrentUser>;
+  /** Long live refresh token */
+  refreshToken: Scalars['String'];
+};
+
+/** Input object for set chat speech style */
+export type SetChatSpeechStyleInput = {
+  /** Chat ID */
+  chatId: Scalars['ID'];
+  /** Speech style */
+  value: SpeechStyle;
+};
+
+/** Set chat speech style payload */
+export type SetChatSpeechStylePayload = {
+  __typename?: 'SetChatSpeechStylePayload';
+  /** Chat */
+  chat: Chat;
+};
+
+/** Input object for set chat tone of voice */
+export type SetChatToneOfVoiceInput = {
+  /** Chat ID */
+  chatId: Scalars['ID'];
+  /** Tone of voice */
+  value: ToneOfVoice;
+};
+
+/** Set chat tone of voice payload */
+export type SetChatToneOfVoicePayload = {
+  __typename?: 'SetChatToneOfVoicePayload';
+  /** Chat */
+  chat: Chat;
 };
 
 /** Input object for sign in */
@@ -362,23 +562,73 @@ export type SignUpPayload = {
   refreshToken: Scalars['String'];
 };
 
-/** Input object for update user password on reset password */
-export type UpdatePasswordInput = {
-  /** A new password */
-  password: Scalars['String'];
-  /** Password reset token */
-  resetToken: Scalars['String'];
+/** Speech style type */
+export enum SpeechStyle {
+  /** Conversational speech style */
+  Conversational = 'CONVERSATIONAL',
+  /** Debate speech style */
+  Debate = 'DEBATE',
+  /** Demonstrative speech style */
+  Demonstrative = 'DEMONSTRATIVE',
+  /** Eulogy or funeeral speech style */
+  EulogyOrFuneeral = 'EULOGY_OR_FUNEERAL',
+  /** Explanatory speech style */
+  Explanatory = 'EXPLANATORY',
+  /** Farewell speech style */
+  Farewell = 'FAREWELL',
+  /** Improptu speech style */
+  Improptu = 'IMPROPTU',
+  /** Informative speech style */
+  Informative = 'INFORMATIVE',
+  /** Intertraining speech style */
+  Intertraining = 'INTERTRAINING',
+  /** Motivational speech style */
+  Motivational = 'MOTIVATIONAL',
+  /** Orartorical speech style */
+  Orartorical = 'ORARTORICAL',
+  /** Persuasive speech style */
+  Persuasive = 'PERSUASIVE',
+  /** Pitch speech style */
+  Pitch = 'PITCH',
+  /** Special ocassion speech style */
+  SpecialOcassion = 'SPECIAL_OCASSION',
+}
+
+/** Tone of voice type */
+export enum ToneOfVoice {
+  /** Assertive */
+  Assertive = 'ASSERTIVE',
+  /** Conversational */
+  Conversational = 'CONVERSATIONAL',
+  /** Formal */
+  Formal = 'FORMAL',
+  /** Humorous */
+  Humorous = 'HUMOROUS',
+  /** Informal */
+  Informal = 'INFORMAL',
+  /** Motivating */
+  Motivating = 'MOTIVATING',
+  /** Optimistic */
+  Optimistic = 'OPTIMISTIC',
+  /** Respectful */
+  Respectful = 'RESPECTFUL',
+  /** Serious */
+  Serious = 'SERIOUS',
+}
+
+/** Input object for update chat mutation */
+export type UpdateChatInput = {
+  /** Chat ID */
+  id: Scalars['ID'];
+  /** Chat name */
+  name?: InputMaybe<Scalars['String']>;
 };
 
-/** Payload object for update password mutation */
-export type UpdatePasswordPayload = {
-  __typename?: 'UpdatePasswordPayload';
-  /** Short live access token */
-  accessToken: Scalars['String'];
-  /** Current User */
-  me?: Maybe<CurrentUser>;
-  /** Long live refresh token */
-  refreshToken: Scalars['String'];
+/** Payload object for update chat mutation */
+export type UpdateChatPayload = {
+  __typename?: 'UpdateChatPayload';
+  /** Chat */
+  chat: Chat;
 };
 
 /** Payload object for update token mutation */
@@ -413,19 +663,4 @@ export type UpdateUserPayload = {
   __typename?: 'UpdateUserPayload';
   /** Current User */
   me: CurrentUser;
-};
-
-/** Base user info */
-export type User = {
-  __typename?: 'User';
-  /** URL to avatar image */
-  avatarUrl?: Maybe<Scalars['String']>;
-  /** Email */
-  email: Scalars['String'];
-  /** First Name */
-  firstName?: Maybe<Scalars['String']>;
-  /** ID */
-  id: Scalars['ID'];
-  /** Last Name */
-  lastName?: Maybe<Scalars['String']>;
 };
